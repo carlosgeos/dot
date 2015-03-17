cb2d295594879132f4f67c1fa686aae42d1ec463
Carlos Requena López <crequena@ulb.ac.be>
2015-03-17 20:37:20 +0100

    modified tramp settings

--- .emacs.d/settings/tramp-settings.el
+++ .emacs.d/settings/tramp-settings.el
@@ -1,4 +1,7 @@
 (setq tramp-default-method "ssh")
 (setq command-line-default-directory "/linode:Documents/")
+(setq tramp-auto-save-directory "~/.emacs.d/tramp-autosave")
+; Make C-c o the general key for switching windows
+(global-set-key (kbd "C-c o") 'other-window)
 
 (provide 'tramp-settings)
