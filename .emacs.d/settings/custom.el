c98729f3ea06f696b4ec7fb6abe88133756ab691
Carlos Requena López <crequena@ulb.ac.be>
2015-06-06 19:07:25 +0200

    changes

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -14,4 +14,3 @@
      ;; Your init file should contain only one such instance.
      ;; If there is more than one, they won't work right.
      '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 120 :width normal))))))
-
