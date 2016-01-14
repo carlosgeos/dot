f724c84de98531b587b8257bd363b7aecc167246
Carlos Requena López <crequena@ulb.ac.be>
2016-01-14 16:17:37 +0100

    Hack font 14pt for emacs (linux and mac)

--- .emacs.d/settings/custom.el
+++ .emacs.d/settings/custom.el
@@ -14,10 +14,9 @@
  '(sml/theme (quote respectful))
  '(tool-bar-mode nil))
 
-(if (system-is-linux)
-    (custom-set-faces
-     ;; custom-set-faces was added by Custom.
-     ;; If you edit it by hand, you could mess it up, so be careful.
-     ;; Your init file should contain only one such instance.
-     ;; If there is more than one, they won't work right.
-     '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 120 :width normal))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 140 :width normal)))))
