4eabd573f947634a02ceccf55d52f003607f5161
Carlos Requena López <crequena@ulb.ac.be>
2017-01-02 23:49:33 +0100

    update maxima version (for macOS)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -160,7 +160,7 @@
 ;; ---------- Major Modes
 
 (when (memq window-system '(mac ns))
-  (add-to-list 'load-path "/usr/local/Cellar/maxima/5.37.2/share/maxima/5.37.2/emacs"))
+  (add-to-list 'load-path "/usr/local/Cellar/maxima/5.38.1/share/maxima/5.38.1/emacs"))
 (use-package imaxima
   :config
   (setq imaxima-fnt-size "Large")
