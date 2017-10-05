eeedfb4360f7384bd013cb22da42962bdde7cf37
Carlos Requena López <crequena@ulb.ac.be>
2017-10-05 13:28:49 +0200

    sphinx mode to write better docs in python

--- .emacs.d/settings/packages/completion.el
+++ .emacs.d/settings/packages/completion.el
@@ -1,3 +1,4 @@
+
 ;; ---------- Programming
 
 (use-package helm
@@ -26,6 +27,14 @@
   :ensure t
   :init
   (global-company-mode)
+  )
+
+(use-package sphinx-doc
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook (lambda ()
+                                  (require 'sphinx-doc)
+                                  (sphinx-doc-mode t)))
   )
 
 (provide 'completion)
