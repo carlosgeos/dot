2cc14d3d3c226d319b2a7f6d5f525a100237deb1
Carlos Requena <crequena@ulb.ac.be>
2018-01-30 00:15:41 +0100

    Indium development environment

--- .emacs.d/settings/packages/misc.el
+++ .emacs.d/settings/packages/misc.el
@@ -5,6 +5,17 @@
   (when (system-is-mac)
     (exec-path-from-shell-initialize)))
 
+(use-package key-chord
+  :ensure t
+  :init
+  (key-chord-mode 1))
+;; (add-hook 'clojure-mode-hook #'(key-chord-mode +1))
+;; (add-hook 'emacs-lisp-mode-hook #'(key-chord-mode +1))
+;; (add-hook 'common-lisp-mode-hook #'(key-chord-mode +1))
+;; (add-hook 'scheme-mode-hook #'(key-chord-mode +1)))
+
+;; Development environments
+
 (use-package cider
   :ensure t
   :init
@@ -12,13 +23,7 @@
   (setq cider-repl-use-pretty-printing t)
   (setq cider-history-file "~/.emacs.d/nrepl-history"))
 
-(use-package key-chord
-  :ensure t
-  :init
-  (key-chord-mode 1))
-  ;; (add-hook 'clojure-mode-hook #'(key-chord-mode +1))
-  ;; (add-hook 'emacs-lisp-mode-hook #'(key-chord-mode +1))
-  ;; (add-hook 'common-lisp-mode-hook #'(key-chord-mode +1))
-  ;; (add-hook 'scheme-mode-hook #'(key-chord-mode +1)))
+(use-package indium
+  :ensure t)
 
-  (provide 'misc)
+(provide 'misc)
