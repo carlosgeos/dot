56c5b4d821b02857daa06d0abaddfac7c582f643
Carlos Requena López <crequena@ulb.ac.be>
2018-03-31 23:15:20 +0200

    add clj-refactor

--- .emacs.d/settings/packages/completion.el
+++ .emacs.d/settings/packages/completion.el
@@ -10,8 +10,7 @@
          ;; From helm maintainer config file, remap instead of simple
          ;; binding
          ("<remap> <list-buffers>" . helm-buffers-list))
-  :config
-  )
+  :config)
 
 (use-package yasnippet
   :ensure t
@@ -31,15 +30,15 @@
 (use-package company
   :ensure t
   :init
-  (global-company-mode)
-  )
+  (global-company-mode))
 
 (use-package sphinx-doc
   :ensure t
   :config
-  (add-hook 'python-mode-hook (lambda ()
-                                  (require 'sphinx-doc)
-                                  (sphinx-doc-mode t)))
-  )
+  :hook python-mode)
+
+(use-package clj-refactor
+  :ensure t
+  :hook clojure-mode)
 
 (provide 'completion)
