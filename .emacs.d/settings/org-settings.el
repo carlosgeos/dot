38afa08b7f0b3d373543fdae392f098cc27fc784
Carlos Requena López <crequena@ulb.ac.be>
2019-01-07 11:44:45 +0100

    change org-noter pdf window size for macos

--- .emacs.d/settings/org-settings.el
+++ .emacs.d/settings/org-settings.el
@@ -16,9 +16,11 @@
 (use-package org-noter
   :ensure t
   :config
-  ;; make pdf larger in linux (using bigger screen)
+  ;; make the pdf side take a bit more space than half the screen
   (when (system-is-linux)
     (setq org-noter-doc-split-fraction '(0.7 . 0.5)))
+  (when (system-is-mac)
+    (setq org-noter-doc-split-fraction '(0.6 . 0.5)))
   ;; make embedded latex appear larger, 1.8 works well
   (plist-put org-format-latex-options :scale 1.8)
   (add-hook 'org-noter-notes-mode-hook 'turn-on-auto-fill))
