6301868a3c785615dfe46880a67a7aa755576637
Carlos Requena López <carlos@requena-lopez.com>
2021-07-14 21:45:22 +0200

    add copy both A and B option to magit-ediff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -560,6 +560,17 @@ are parameters of 'kill-ring-save'."
   :config
   (setq transient-default-level 5))
 
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
 (use-package kubernetes
   :ensure t
   :bind ("C-x t" . kubernetes-overview))
