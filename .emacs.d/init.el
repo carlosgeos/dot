c597c7ba1349607ff0a6d605795bba8261b7a338
Carlos Requena López <carlos@requena-lopez.com>
2024-02-17 15:15:17 +0100

    add note about `company-idle-delay` and LSP

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,9 @@
 
 ;;; Code:
 
+;; Increase startup gc-threshold
+(setq gc-cons-threshold 100000000)      ;100MB
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Bootstrap straight.el                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -70,13 +73,13 @@
 ;; Helper functions ;;
 ;;;;;;;;;;;;;;;;;;;;;;
 
-(defun system-is-mac ()
-  "Return non-nil if system is mac."
-  (string-equal system-type "darwin"))
+(defconst *sys/linux*
+  (eq system-type 'gnu/linux)
+  "Are we running on a GNU/Linux system?")
 
-(defun system-is-linux ()
-  "Return non-nil if system is linux."
-  (string-equal system-type "gnu/linux"))
+(defconst *sys/mac*
+  (eq system-type 'darwin)
+  "Are we running on a Mac system?")
 
 (defun untabify-except-makefiles ()
   "Replace tabs with spaces except in makefiles."
@@ -253,7 +256,7 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
 (global-set-key (kbd "C-c o") 'other-window)
 
 ;; set command key to be meta instead of option
-(when (system-is-mac)
+(when *sys/mac*
   (customize-set-variable 'ns-command-modifier 'meta)
   ;; ar and ranlib should come from LLVM and not GNU to be able to
   ;; compile pdf-tools on mac (true as of Jan 2019)
@@ -289,10 +292,10 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
 ;; (use-package org-noter
 ;;   :config
 ;;   ;; sizing adjustments depending on the screen used
-;;   (when (system-is-linux)
+;;   (when *sys/linux*
 ;;     (setq org-noter-doc-split-fraction '(0.7 . 0.5))
 ;;     (plist-put org-format-latex-options :scale 2.0))
-;;   (when (system-is-mac)
+;;   (when *sys/mac*
 ;;     (setq org-noter-doc-split-fraction '(0.55 . 0.5))
 ;;     (plist-put org-format-latex-options :scale 1.3))
 ;;   (add-hook 'org-noter-notes-mode-hook 'turn-on-auto-fill)
@@ -315,8 +318,7 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
   :config
   (define-clojure-indent
    (forcat 1)
-   (defsc :defn))
-  (require 'flycheck-clj-kondo))
+   (defsc :defn)))
 
 (use-package web-mode
   :init
@@ -357,7 +359,7 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
   :straight auctex
   :init
   (add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
-  (add-hook 'LaTeX-mode-hook (lambda () (abbrev-mode +1))))
+  (add-hook 'LaTeX-mode-hook (lambda () (abbrev-mode 1))))
 
 (use-package markdown-mode
   :mode
@@ -384,17 +386,6 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
 ;;;;;;;;;;;;;;
 ;; LSP mode ;;
 ;;;;;;;;;;;;;;
-
-;;; Performance tweaks
-
-;; Around 2MB is the sweetspot.
-;; This is also an option: http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
-;; Setting it to a big number like 10MB or 100MB is not a good
-;; idea. When it does actually garbage collect, Emacs will freeze
-;; during a second or two
-(setq gc-cons-threshold 2000000)        ;around 2MB
-(setq read-process-output-max (* 1024 1024 2))
-(setq max-lisp-eval-depth 3200)
 
 (use-package lsp-mode
   :init
@@ -441,7 +432,9 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
   (setq cider-repl-pop-to-buffer-on-connect nil)
   (setq cider-repl-use-pretty-printing t)
   (setq cider-repl-history-file "~/.emacs.d/nrepl-history")
-  :bind ("C-c <tab>" . cider-format-defun))
+  (setq cider-connection-message-fn nil)
+  :config
+  (cider-enable-flex-completion))
 
 (use-package ess
   ;; ESS needs aggressive scroll on the inferior interactive
@@ -492,7 +485,9 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
   :bind
   (("M-g a" . helm-do-grep-ag)))
 
-(use-package anzu)
+(use-package anzu
+  :init
+  (global-anzu-mode 1))
 
 (use-package yasnippet
   :init
@@ -508,14 +503,18 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
 ;; necessary
 (use-package yasnippet-snippets)
 
-;; (use-package company
-;;   :init
-;;   (global-company-mode)
-;;   :config
-;;   (setq company-idle-delay 0.1))
-
-;; (add-to-list 'company-backends 'company-irony)
-;; (add-to-list 'company-backends 'company-c-headers)
+(use-package company
+  :init
+  (global-company-mode)
+  :config
+  ;; Anything lower than 0.3 doesn't work (hangs often) in a big
+  ;; project, when LSP provides a `completion-at-point` fn to
+  ;; `company-capf`. In those cases, 'lsp-completion-enable' should be
+  ;; set to 'nil' in .dir-locals
+  ;;
+  ;; 'company-dabbrev' (simple completion in buffer) remains fast with
+  ;; a 0.15 idle delay
+  (setq company-idle-delay 0.15))
 
 (use-package clj-refactor
   :diminish clj-refactor-mode
@@ -531,7 +530,7 @@ BUFFER and ALIST are passed from `display-buffer-alist`"
   (setq projectile-completion-system 'helm)
   (setq projectile-mode-line-function
         '(lambda () (format " Proj[%s]" (projectile-project-name))))
-  (projectile-mode +1))
+  (projectile-mode 1))
 
 (use-package helm-projectile
   :config
@@ -562,8 +561,6 @@ A and then B."
 
 (use-package flycheck-irony)
 
-(use-package flycheck-clj-kondo)
-
 (use-package flycheck
   :config
   ;; Chain checkers together
@@ -590,7 +587,7 @@ A and then B."
 ;;; buffer. This flag makes the *maxima* buffer use imaxima under the
 ;;; hood. M-x imaxima should be run first though !
 (setq imaxima-use-maxima-mode-flag t)
-(when (system-is-mac)
+(when *sys/mac*
   (setq imaxima-scale-factor 1.3))
 
 ;;; Misc
@@ -616,6 +613,17 @@ A and then B."
 ;;   (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 ;;   (add-hook 'common-lisp-mode-hook #'aggressive-indent-mode)
 ;;   (add-hook 'scheme-mode-hook #'aggressive-indent-mode))
+
+;;; Performance tweaks
+
+;; Around 2MB is the sweetspot.
+;; This is also an option: http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+;; Setting it to a big number like 10MB or 100MB is not a good
+;; idea. When it does actually garbage collect, Emacs will freeze
+;; during a second or two
+(setq gc-cons-threshold 3000000)        ;around 3MB
+(setq read-process-output-max (* 1024 1024 2))
+(setq max-lisp-eval-depth 3200)
 
 (provide 'init)
 
