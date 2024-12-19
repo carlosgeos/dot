4850fa43943c4dfa908d9f083336c6130bd72874
Carlos Requena López <carlos@requena-lopez.com>
2024-12-19 16:37:22 +0100

    bump `cider-nrepl`

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:dependencies [[vvvvalvalval/scope-capture "0.3.3"]]
         :injections [(require 'sc.api)]}
- :repl {:plugins [[cider/cider-nrepl "0.50.2"]
+ :repl {:plugins [[cider/cider-nrepl "0.50.3"]
                   [refactor-nrepl "3.10.0"] ]}}
